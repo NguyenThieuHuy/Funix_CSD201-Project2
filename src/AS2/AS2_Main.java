7ea224e0236e4ac53996c2ea3a32443079994f1a
Thieu Huy Nguyen <86967333+NguyenThieuHuy@users.noreply.github.com>
2022-05-18 22:34:53 +0700

    switcher

--- src/AS2/AS2_Main.java
+++ src/AS2/AS2_Main.java
@@ -36,9 +36,50 @@ public class AS2_Main {
     
    
     public static void main(String[] args) {
-   
-    
-   
+      Scanner sc = new Scanner(System.in);
+      showMenu();
+      int key = sc.nextInt();
+      boolean repeate = true;
+      do {
+        switch (key) {
+          case 1:
+            break;
+  
+          case 2:
+            break;
+  
+          case 3:
+            break;
+  
+          case 4:
+            break;
+  
+          case 5:
+            break;
+  
+          case 6:
+            break;
+  
+          case 7:
+            break;
+  
+          case 8:
+            break;
+  
+          case 9:
+            break;
+  
+          case 10:
+            break;
+  
+          case 0:
+            break;
+  
+          default:
+            repeate = false;
+            break;
+        }
+      } while (repeate = true);
+      sc.close();
     }
-   
     }
